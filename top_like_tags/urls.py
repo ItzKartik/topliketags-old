8f58838836fc912043f08dbeb87482f42d24c31d
Kartik <smartzboi@gmail.com>
2020-06-04 19:46:36 +0530

    make it better

--- top_like_tags/urls.py
+++ top_like_tags/urls.py
@@ -10,8 +10,9 @@ app_name = 'top_like_tags'
 urlpatterns = [
     path('', views.index, name='index'),
     path('popular_hashtags/', views.fixed, name='fixed_hashtag'),
-    path('forums/', views.forums, name='forums'),
+    path('hashtag_tips/', views.forums, name='forums'),
     path('about/', TemplateView.as_view(template_name='top_like_tags/about.html'), name='about'),
+    path('policy/', TemplateView.as_view(template_name='top_like_tags/policy.html'), name='policy'),
     path('contact/', views.contact, name='contact'),
     re_path('blog/(?P<blog_id>[\w-]+)', views.full_blog, name='full_blog'),
     path('generator/', views.generator.as_view(), name='generator'),
