70c330779f658e52ffc512fe77e86cf76ecaf742
itzkartik <smartzboi@gmail.com>
2020-06-24 14:34:50 +0530

    make

---
+++ top_like_tags/insta_login.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from webdriver_manager.chrome import ChromeDriverManager
+
+chromeOptions = Options()
+# chromeOptions.add_argument("--headless")
+# chromeOptions.add_argument('--disable-gpu')
+# chromeOptions.add_argument("--disable-dev-sha-usage")
+# chromeOptions.add_argument("--no-sandbox")
+
+drivers = []
+def insta_login():    
+    if drivers != []:
+        return drivers
+    else:   
+        x = webdriver.Chrome(ChromeDriverManager().install(), options=chromeOptions)
+        x.maximize_window()
+        drivers.append(x)
+        x.get("https://instagram.com")
+        sleep(2)
+        x.find_element_by_name("username").send_keys("3aceboyz")
+        x.find_element_by_name("password").send_keys("Kartik@777")
+        sleep(2)
+        try:
+            x.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button').click()
+        except NoSuchElementException:
+            x.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button').click()
+        sleep(2)
+        j = 0
+        try:
+            if j == 0:
+                captcha_present = x.find_element_by_xpath(
+                    '//*[@id="react-root"]/section/main/div[2]/div/div/div[2]/div/button')
+                print("Captcha Locked")
+                x.close()
+        except NoSuchElementException:
+            print("Logged In")
+        return drivers
+
